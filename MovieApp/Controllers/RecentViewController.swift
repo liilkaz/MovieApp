f8f18f9a58b4fcd644b3d74a5172a1b90a76f7ba
Fazil Jabbarov <faziljabbarov1998@gmail.com>
2023-04-09 14:05:20 +0300

    RecentViewController and TableView

--- MovieApp/Controllers/RecentViewController.swift
+++ MovieApp/Controllers/RecentViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class RecentViewController: UIViewController {
 
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "BgColor")
         title = Constants.Titles.NavBar.recent
         navigationController?.tabBarItem.title = Constants.Titles.TabBar.title(for: .recent)
+        tableView.rowHeight = 160
+        view.addSubview(tableView)
+        setConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(RecentTableViewCell.self, forCellReuseIdentifier: RecentTableViewCell.identifier)
     }
 
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSourse
+
+extension RecentViewController: UITableViewDelegate, UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 180
+        }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecentTableViewCell.identifier,
+                                                       for: indexPath) as? RecentTableViewCell else { return UITableViewCell() }
+       
+        return cell
+    }
+    
 }

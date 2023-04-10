46d2cedfc186f2ac8f392428c3db9571f5b77e2f
alina <art.alina73@mail.ru>
2023-04-10 20:10:00 +0300

    setting screen

--- MovieApp/Controllers/SettingViewController.swift
+++ MovieApp/Controllers/SettingViewController.swift
@@ -7,13 +7,166 @@
 
 import UIKit
 
+
 class SettingViewController: UIViewController {
 
+    enum MyConstants {
+        static let logOutButtonImage: String = "logOutButton"
+        static let userPhotoImage: String = "userPhoto"
+        static let userNameText: String = "Andy Lexsian"
+        static let userNicknameText: String = "@Andy1999"
+    }
+    
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = MyConstants.userNameText
+        label.font = UIFont(name: "PlusJakartaSans-SemiBold", size: 18)
+        label.textColor = UIColor(named: "MainTextColor")
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var userNicknameLabel: UILabel = {
+        let label = UILabel()
+        label.text = MyConstants.userNicknameText
+        label.font = UIFont(name: "PlusJakartaSans-SemiBold", size: 14)
+        label.textColor = UIColor(named: "MainTextColor")
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var userPhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: MyConstants.userPhotoImage)
+        return imageView
+    }()
+    
+    private lazy var userInformationStackView = UIStackView()
+    
+    private lazy var logOutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(named: MyConstants.logOutButtonImage), for: .normal)
+        button.addTarget(self, action: #selector(logOutButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func logOutButtonTapped() {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: idCell)
+        tableView.register(SettingHeaderTableViewCell.self, forHeaderFooterViewReuseIdentifier: idHeader)
+        
         view.backgroundColor = UIColor(named: "BgColor")
         title = Constants.Titles.NavBar.setting
         navigationController?.tabBarItem.title = Constants.Titles.TabBar.title(for: .setting)
+        setupViews()
+        setConstraints()
     }
     
+    private func setupViews() {
+        
+        userInformationStackView = UIStackView(arrangedSubviews: [
+            userNameLabel,
+            userNicknameLabel
+        ], axis: .vertical, spacing: 2)
+        
+        view.addSubview(userPhotoImageView)
+        view.addSubview(userInformationStackView)
+        view.addSubview(tableView)
+        view.addSubview(logOutButton)
+    }
+  
+    private func setConstraints() {
+        userInformationStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userInformationStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            userInformationStackView.leadingAnchor.constraint(equalTo: userPhotoImageView.trailingAnchor, constant: 12),
+            userInformationStackView.widthAnchor.constraint(equalToConstant: 115),
+            userInformationStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        userPhotoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userPhotoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 37),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 56),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 56)
+        ])
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: userPhotoImageView.bottomAnchor, constant: 32),
+            tableView.bottomAnchor.constraint(equalTo: logOutButton.topAnchor, constant: -5),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        ])
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logOutButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -95),
+            logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+// TABLE
+    let idCell = "idCell"
+    let idHeader = "idHeader"
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.bounces = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
 }
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return 3
+        default: break
+        }
+        return 0
+    }
+     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idCell, for: indexPath) as? SettingTableViewCell
+        cell?.cellConfigure(indexPath: indexPath)
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: idHeader) as? SettingHeaderTableViewCell
+        header?.headerConfigure(section: section)
+        return header ?? SettingHeaderTableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+
---
+++ MovieApp/Views/SettingScreen/SettingTableViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  SettingTableViewCell.swift
+//  MovieApp
+//
+//  Created by Alina Artamonova on 10.04.2023.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+    let cellNameArray = [["Profile"],
+                     ["Change Password", "Change Password", "Dark Mode"]]
+    
+    let cellSettingImageArray = [["person"],
+                     ["lock", "unlock", "darkMode"]]
+    
+    private lazy var nameSetting: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(named: "MainTextColor")
+        label.font = UIFont(name: "PlusJakartaSans-SemiBold", size: 16)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var settingImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var vectorImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var reapeatSwitch: UISwitch = {
+        let reapeatSwitch = UISwitch()
+        reapeatSwitch.isOn = true
+        reapeatSwitch.isHidden = true
+        reapeatSwitch.translatesAutoresizingMaskIntoConstraints = false
+        return reapeatSwitch
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setConstraints()
+
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+        reapeatSwitch.addTarget(self, action: #selector(switchChange(paramTarget:)), for: .valueChanged)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("alina")
+    }
+    
+    func cellConfigure(indexPath: IndexPath) {
+        nameSetting.text = cellNameArray[indexPath.section][indexPath.row]
+        settingImage.image = UIImage(named: cellSettingImageArray[indexPath.section][indexPath.row])
+        if indexPath == [2, 2] {
+            
+        }
+        if indexPath == [1, 2] {
+            reapeatSwitch.isHidden = false
+        }
+        if indexPath == [0, 0] {
+            vectorImage.image = UIImage(named: "vector")
+        }
+    }
+    
+    @objc func switchChange(paramTarget: UISwitch) {
+        
+    }
+    
+    func setConstraints() {
+        
+        var cellInformationStackView: UIStackView = {
+            let stackView = UIStackView ()
+            stackView.addArrangedSubview(settingImage)
+            stackView.addArrangedSubview(nameSetting)
+            stackView.axis = .horizontal
+            stackView.spacing = 12
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+        self.addSubview(cellInformationStackView)
+        NSLayoutConstraint.activate([
+            cellInformationStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            cellInformationStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24)
+        ])
+        self.contentView.addSubview(vectorImage)
+        NSLayoutConstraint.activate([
+            vectorImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            vectorImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30)
+        ])
+        self.contentView.addSubview(reapeatSwitch)
+        NSLayoutConstraint.activate([
+            reapeatSwitch.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            reapeatSwitch.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15)
+        ])
+    }
+}
+

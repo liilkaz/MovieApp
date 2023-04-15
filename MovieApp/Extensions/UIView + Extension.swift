faa55ad68e02385929f94a67ec69df7085cc0814
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-04-15 10:47:23 +0500

    добавил расширение для анимации картинки в индикаторе активности

--- MovieApp/Extensions/UIView + Extension.swift
+++ MovieApp/Extensions/UIView + Extension.swift
@@ -20,4 +20,23 @@ extension UIView {
         }
         NSLayoutConstraint.activate(constraints)
     }
+    
+    //MARK: - Activity Indicator
+
+    func startAnimationLoading() {
+        let rotation = CABasicAnimation(keyPath: "transform.rotation")
+        self.isHidden = false
+        rotation.fromValue = 0
+        rotation.toValue = 2 * Double.pi
+        rotation.duration = 0.7
+        rotation.repeatCount = 100
+        rotation.isRemovedOnCompletion = false
+        layer.add(rotation, forKey: "spin")
+        rotation.isRemovedOnCompletion = false
+    }
+
+    func stopAnimationLoading() {
+        self.isHidden = true
+        layer.removeAllAnimations()
+    }
 }
